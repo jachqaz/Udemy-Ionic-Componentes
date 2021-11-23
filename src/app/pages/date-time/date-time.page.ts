0191da5cfbafb078fd67fcb70972eef8899e69b7
Johan Castano <jach_1997@hotmail.com>
2021-11-23 00:21:32 -0500

    012 ion-datetime - Parte 2 - Personalización
    Signed-off-by: Johan Castano <jach_1997@hotmail.com>

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -7,7 +7,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DateTimePage implements OnInit {
 
+  customYearValues = [2020, 2016, 2008, 2004, 2000, 1996];
+
   fechaNaci: Date = new Date();
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Hola',
+        handler: (event) => {
+          console.log(event);
+        }
+      },
+      {
+        text: 'Mundo',
+        handler: () => {
+          console.log('Log');
+        }
+      },
+    ]
+  };
 
   constructor() {
   }
